e02d5930c185ef426b535178d4077458908eed96
godruoyi <g@godruoyi.com>
2016-06-08 00:22:46 +0800

    one

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 import BaseController from '../service/BaseController'
 // import Crawler from './../service/Crawler.js';
-import doWork from './../service/Demo.js';
-import phantom2 from './../service/phantom.js';
+// import doWork from './../service/Demo.js';
+// import phantom2 from './../service/phantom.js';
+import phantom3 from './../service/Test.js';
 
 
 export default class extends BaseController{
 	reoutes(){
-		this.router.get('/crawler', async function(ctx, next){
+		/*this.router.get('/crawler', async function(ctx, next){
 
 			// Crawler('http://open.iot.10086.cn');
 			
@@ -23,10 +24,13 @@ export default class extends BaseController{
 
 			let xxx = phantom2('http://open.iot.10086.cn');
 			console.log(xxx);
+		});*/
 
+		this.router.get('/doWork3', async function(ctx, next){
 
-			
-			
+			let xxx = phantom3('http://open.iot.10086.cn');
+			console.log('-----------------route--------------------');
+			console.log(xxx);
 		});
 	};
 }
--- service/utils/UrlQueues.js
+++ service/utils/UrlQueues.js
@@ -1,13 +1,35 @@
 export default class {
 	
-	constructor(router)
+	constructor()
 	{
 		this.queue = [];
+		this.queue_p = [];
+
 	}
 
 	push (data)
 	{
-		
+		let l = data.length;
+		if( (l + this.getLength()) >= 200)
+		{
+			data = data.slice(0, (201 - this.getLength()));
+		}
+		for(let a of data){
+			if(!this.queue.includes(a)){
+				this.queue.push(a);
+				this.queue_p.push(a);
+			}
+		}
+	}
+
+	next()
+	{
+		let done = this.queue_p.shift();
+		if( typeof(done) === 'undefined')
+		{
+			return false;
+		}
+		return done;
 	}
 
 
